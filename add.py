a521ad377fd6b74eb5416c3a7f28a0c3fdac5b20
TejasKapare <96877790+TejasKapare@users.noreply.github.com>
2023-05-29 13:50:29 +0530

    Update add.py

--- add.py
+++ add.py
@@ -1,4 +1,4 @@
-a = input("Enter a number:")
-b = input("Enter another number:")
+a = int(input("Enter a number:"))
+b = int(input("Enter another number:"))
 c = a+b
 print("Addition of two numbers is : " + c)
